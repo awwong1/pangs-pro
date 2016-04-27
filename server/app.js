74c79c48f8a4c4d51447ac578ee0fa147bfed5ec
Alexander Wong <admin@alexander-wong.com>
2016-04-26 19:29:22 -0600

    Added robots file, made facebook link go to client facebook page

--- server/app.js
+++ server/app.js
@@ -7,8 +7,13 @@ var env = process.env;
 var app = express();
 app.use(helmet());
 
+// serve all static files inside the client directory
 app.use(express.static('client'));
 
+// health probe check for OpenShift
+app.get('/health', function (req, res) {
+  res.send('1');
+});
 
 app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
